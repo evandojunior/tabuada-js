f6b9e2b56621d4960d0249369a65a6d803bcf79f
Evando Junior <evando.junior@live.com>
2017-11-03 14:55:13 -0200

    update function

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 function changeOperation(signal, num = null) {
   if (!num) {
-    for (var i = num; i <= 10; i++) {
+    for (var i = 1; i <= 10; i++) {
       calculate(i, signal)
       renderTab()
     }
@@ -10,21 +10,35 @@ function changeOperation(signal, num = null) {
 }
 
 function calculate(i, signal) {
+  var table = createTable()
   for (var j = 1; j <= 10; j++) {
-    renderResult(i + signal + j + " = " + eval(i + signal + j))
+    var result = i + signal + j + " = "  + arred(eval(i + signal + j), 2)
+    renderResult(table, result)
   }
 }
 
+function arred(d,casas) {
+   var aux = Math.pow(10,casas)
+   return Math.floor(d * aux)/aux
+}
+
 function renderTab() {
-  document.getElementById('tabuada').children[0].innerHTML += "<hr>";
+  // document.getElementById('tabuada').innerHTML += "<hr>";
 }
 
 function clearTab() {
-  document.getElementById('tabuada').children[0].innerHTML = ""
+  document.getElementById('tabuada').innerHTML = ""
 }
 
-function renderResult(result) {
-  document.getElementById('tabuada').children[0].innerHTML += "<tr><td>" + result + "</td></tr>"
+function createTable() {
+  var table = document.createElement('table')
+  table.setAttribute('class', 'my-class')
+  document.getElementById('tabuada').appendChild(table);
+  return table
+}
+
+function renderResult(table, result) {
+  table.innerHTML += "<tr><td>" + result + "</td></tr>"
 }
 
 document.getElementById('operacao').addEventListener('change', function() {
